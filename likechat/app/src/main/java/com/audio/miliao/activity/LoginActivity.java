0146f06d5a333c8caa9670567dd5234d06702dcf
liujiye <jiye.liu@imduoduo.com>
2017-05-26 19:08:43 +0800

    添加支付宝支付

--- likechat/app/src/main/java/com/audio/miliao/activity/LoginActivity.java
+++ likechat/app/src/main/java/com/audio/miliao/activity/LoginActivity.java
@@ -3,13 +3,19 @@ package com.audio.miliao.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.alipay.sdk.app.PayTask;
 import com.audio.miliao.R;
 import com.audio.miliao.entity.UserInfo;
 import com.audio.miliao.http.HttpUtil;
 import com.audio.miliao.http.cmd.Login;
+import com.audio.miliao.pay.alipay.Constant;
+import com.audio.miliao.pay.alipay.OrderInfoUtil2_0;
+import com.audio.miliao.pay.alipay.PayResult;
 import com.audio.miliao.theApp;
 import com.audio.miliao.util.QQUtil;
 import com.audio.miliao.util.WXUtil;
@@ -21,6 +27,10 @@ import com.uikit.loader.LoaderApp;
 import com.uikit.loader.entity.LoaderAppData;
 import com.uikit.loader.service.YXService;
 
+import java.util.Map;
+
+import static com.audio.miliao.pay.alipay.Constant.RSA2_PRIVATE;
+
 
 /**
  * 登录
@@ -29,6 +39,7 @@ public class LoginActivity extends BaseActivity
 {
     private static final int CODE_QQ_LOGIN = 0;
     private static final int CODE_WEIXIN_LOGIN = 1;
+    private static final int CODE_ALIPAY_SDK_PAY_FLAG = 3;
 
     private EditText mEdittext;
     private YXService mService;
@@ -77,6 +88,29 @@ public class LoginActivity extends BaseActivity
                 theApp.showToast("login failed");
             }
             break;
+        case CODE_ALIPAY_SDK_PAY_FLAG:
+
+            @SuppressWarnings("unchecked")
+            PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+            /**
+             * 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+             */
+            String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+            String resultStatus = payResult.getResultStatus();
+            // 判断resultStatus 为9000则代表支付成功
+            if (TextUtils.equals(resultStatus, "9000"))
+            {
+                // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                //Toast.makeText(getApplicationContext(), "支付成功", Toast.LENGTH_SHORT).show();
+                theApp.showToast("支付成功");
+            }
+            else
+            {
+                // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                //Toast.makeText(getApplicationContext(), "支付失败", Toast.LENGTH_SHORT).show();
+                theApp.showToast("支付失败");
+            }
+            break;
         }
     }
 
@@ -107,6 +141,9 @@ public class LoginActivity extends BaseActivity
                         case R.id.img_back:
                             finish();
                             break;
+                        case R.id.btn_pay_alipay:
+                            onAlipay();
+                            break;
                         }
                     }
                     catch (Exception e)
@@ -120,6 +157,7 @@ public class LoginActivity extends BaseActivity
             findViewById(R.id.txt_qq_login).setOnClickListener(clickListener);
             findViewById(R.id.btn_yunxin_login).setOnClickListener(clickListener);
             findViewById(R.id.img_back).setOnClickListener(clickListener);
+            findViewById(R.id.btn_pay_alipay).setOnClickListener(clickListener);
         }
         catch (Exception e)
         {
@@ -193,5 +231,43 @@ public class LoginActivity extends BaseActivity
         {
             e.printStackTrace();
         }
+    }
+
+    private void onAlipay()
+    {
+
+        /**
+         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
+         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
+         * 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
+         *
+         * orderInfo的获取必须来自服务端；
+         */
+        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
+        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(Constant.APPID, rsa2);
+        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
+
+        String privateKey = rsa2 ? Constant.RSA2_PRIVATE : Constant.RSA_PRIVATE;
+        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
+        final String orderInfo = orderParam + "&" + sign;
+
+        Runnable payRunnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                PayTask alipay = new PayTask(LoginActivity.this);
+                Map<String, String> result = alipay.payV2(orderInfo, true);
+                Log.i("msp", result.toString());
+
+                Message msg = new Message();
+                msg.what = CODE_ALIPAY_SDK_PAY_FLAG;
+                msg.obj = result;
+                handler().sendMessage(msg);
+            }
+        };
+
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
     }
 }
