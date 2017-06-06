6390ac1a725f41030043ef36b2ddc611323d5896
liujiye <jiye.liu@imduoduo.com>
2017-06-06 12:24:10 +0800

    添加 greenDao

---
+++ likechat/app/src/main/java/com/audio/miliao/util/DBUtil.java
@@ -0,0 +1,136 @@
+package com.audio.miliao.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.audio.miliao.entity.DaoMaster;
+import com.audio.miliao.entity.DaoSession;
+import com.audio.miliao.entity.Test;
+import com.audio.miliao.entity.TestDao;
+import com.audio.miliao.theApp;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.util.List;
+
+/**
+ * 操作工具类
+ */
+public class DBUtil
+{
+    private final static String dbName = "test_db";
+    private static DaoMaster.DevOpenHelper openHelper;
+    private static Context mContext;
+
+    private static void init()
+    {
+        mContext = theApp.CONTEXT;
+        if (openHelper == null)
+        {
+            openHelper = new DaoMaster.DevOpenHelper(mContext, dbName, null);
+        }
+    }
+
+    /**
+     * 获取可读数据库
+     */
+    private static SQLiteDatabase readableDatabase()
+    {
+        init();
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+        return db;
+    }
+
+    /**
+     * 获取可写数据库
+     */
+    private static SQLiteDatabase writableDatabase()
+    {
+        init();
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        return db;
+    }
+
+    /**
+     * 插入一条记录
+     *
+     * @param test
+     */
+    public static void insertTest(Test test)
+    {
+        DaoMaster daoMaster = new DaoMaster(writableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        userDao.insert(test);
+    }
+
+    /**
+     * 插入用户集合
+     *
+     * @param tests
+     */
+    public static void insertTestList(List<Test> tests)
+    {
+        if (tests == null || tests.isEmpty())
+        {
+            return;
+        }
+        DaoMaster daoMaster = new DaoMaster(writableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        userDao.insertInTx(tests);
+    }
+
+    /**
+     * 删除一条记录
+     *
+     * @param test
+     */
+    public static void deleteTest(Test test)
+    {
+        DaoMaster daoMaster = new DaoMaster(writableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        userDao.delete(test);
+    }
+
+    /**
+     * 更新一条记录
+     *
+     * @param test
+     */
+    public static void updateTest(Test test)
+    {
+        DaoMaster daoMaster = new DaoMaster(writableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        userDao.update(test);
+    }
+
+    /**
+     * 查询用户列表
+     */
+    public static List<Test> queryTestList()
+    {
+        DaoMaster daoMaster = new DaoMaster(readableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        QueryBuilder<Test> qb = userDao.queryBuilder();
+        List<Test> list = qb.list();
+        return list;
+    }
+
+    /**
+     * 查询用户列表
+     */
+    public static List<Test> queryTestList(int age)
+    {
+        DaoMaster daoMaster = new DaoMaster(readableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        TestDao userDao = daoSession.getTestDao();
+        QueryBuilder<Test> qb = userDao.queryBuilder();
+        qb.where(TestDao.Properties.Age.gt(age)).orderAsc(TestDao.Properties.Age);
+        List<Test> list = qb.list();
+        return list;
+    }
+}
