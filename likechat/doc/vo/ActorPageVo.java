566939dc73dfd425ad646044b24d4b2f35a6db82
suntao <suntao@tigerjoys.com>
2017-07-28 18:16:04 +0800

    ActorPageVo.java 增加两个属性，用于在点击连线主播时，支持相关的页面显示 private String platformPrice;//平台费用，用于点击连线时显示的平台价格； private String totalPrice;//总计费用

--- likechat/doc/vo/ActorPageVo.java
+++ likechat/doc/vo/ActorPageVo.java
@@ -24,8 +24,10 @@ public class ActorPageVo {
 	
 	private Integer voiceSec;//
 	
-	private String price;//资费价格（整数）转成 --》 1.5币/分
-	private String callTime;//通话时长
+	private String price;//资费价格（整数）转成 --》 15币/分
+	private String platformPrice;//平台费用，用于点击连线时显示的平台价格；
+	private String totalPrice;//总计费用
+ 	private String callTime;//通话时长
 	
 	private Boolean isAttention;//是否已经关注，如果未登陆，点击关注应该进入登陆页面。如果已经登陆，当此属性为true，图标应显示红色
 	
@@ -176,6 +178,22 @@ public class ActorPageVo {
 
 	public void setVoiceSec(Integer voiceSec) {
 		this.voiceSec = voiceSec;
+	}
+
+	public String getPlatformPrice() {
+		return platformPrice;
+	}
+
+	public void setPlatformPrice(String platformPrice) {
+		this.platformPrice = platformPrice;
+	}
+
+	public String getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(String totalPrice) {
+		this.totalPrice = totalPrice;
 	}
 	
 }
